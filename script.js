3151cdd53a34bd77139989ebc8ef8005ab5d0a06
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-08-08 11:16:18 -0300

    Concluído requisito 5

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ let cartaGerada = document.getElementById('carta-gerada');
 //2 - Crie um parágrafo com a tag p e ID carta-gerada onde a pessoa usuária verá o resultado de sua carta misteriosa.
 
 //3 - Crie um botão com ID criar-carta que deve gerar a carta misteriosa ao ser clicado.
-let palavrasArray = [];
+
 function gerandoCarta () {
+    let palavrasArray = [];
     cartaGerada.innerHTML = '';
     let  textoValue= inputText.value;
+    // if (textoValue.length < 1) {
+    //     alert('Por favor, digite o conteúdo da carta');      
+    // }
+    if (!textoValue.replace(/\s/g, '').length) {
+        cartaGerada.innerHTML ='Por favor, digite o conteúdo da carta.';
+      }
     palavrasArray = textoValue.split(" ");
     console.log(palavrasArray);
     for (index = 0; index < palavrasArray.length; index ++) {        
