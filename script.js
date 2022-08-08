4fdbc3e6cce4b5b83a8da5302550eb3efa7b897d
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-08-08 12:05:12 -0300

    Concluído requisito 17

--- script.js
+++ script.js
@@ -16,19 +16,39 @@ function gerandoCarta () {
     let palavrasArray = [];
     cartaGerada.innerHTML = '';
     let  textoValue= inputText.value;
-    // if (textoValue.length < 1) {
-    //     alert('Por favor, digite o conteúdo da carta');      
-    // }
+    //O código segunte foi refatorado com base no conteúdo deste lnk: https://stackoverflow.com/questions/10261986/how-to-detect-string-which-contains-only-spaces  
+    //Ele identifica se o value do input está vazio ou se contém somente "espaços" e realizar uma ação confome determinado.
     if (!textoValue.replace(/\s/g, '').length) {
         cartaGerada.innerHTML ='Por favor, digite o conteúdo da carta.';
       }
+    //Fim do código refatorado
     palavrasArray = textoValue.split(" ");
     console.log(palavrasArray);
     for (index = 0; index < palavrasArray.length; index ++) {        
         let spanTexto = document.createElement('span')
         spanTexto.innerText = palavrasArray[index]
+        spanTexto.classList.add(arrayEstilo[Math.floor(Math.random() * arrayEstilo.length)]);
+        spanTexto.classList.add(arrayTamanho[Math.floor(Math.random() * arrayTamanho.length)]);
+        spanTexto.classList.add(arrayRotacao[Math.floor(Math.random() * arrayRotacao.length)]);
+        spanTexto.classList.add(arrayInclinacao[Math.floor(Math.random() * arrayInclinacao.length)]);
         cartaGerada.appendChild(spanTexto);
     }
 }
 
 btnCriarCarta.addEventListener('click', gerandoCarta)
+
+//Requisito 17 - Adicione a possibilidade de alterar o estilo de uma palavra específica ao clicar nela.
+
+function personalizarPalavra () {
+
+    cartaGerada.addEventListener('click', function (event) {
+        let elementoSelecionado = event.target;
+        elementoSelecionado.className = '';
+        elementoSelecionado.classList.add(arrayEstilo[Math.floor(Math.random() * arrayEstilo.length)]);
+        elementoSelecionado.classList.add(arrayTamanho[Math.floor(Math.random() * arrayTamanho.length)]);
+        elementoSelecionado.classList.add(arrayRotacao[Math.floor(Math.random() * arrayRotacao.length)]);
+        elementoSelecionado.classList.add(arrayInclinacao[Math.floor(Math.random() * arrayInclinacao.length)]);
+    })
+}
+personalizarPalavra ();
+
