6096432fb2303187f4e3801e0c17d8cc99ace4a6
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-08-08 11:30:46 -0300

    Concluídos requisitos 6 a 11

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let inputText = document.getElementById('carta-texto');
 let btnCriarCarta = document.getElementById('criar-carta');
 let cartaGerada = document.getElementById('carta-gerada');
+let arrayEstilo = ['newspaper', 'magazine1' , 'magazine2'];
+let arrayTamanho = ['medium', 'big', 'reallybig'];
+let arrayRotacao = ['rotateleft', 'rotateright'];
+let arrayInclinacao = ['skewleft', 'skewright'];
 
 //1 - Crie um input com o ID carta-texto onde a pessoa usuária poderá digitar o conteúdo da carta.
 
